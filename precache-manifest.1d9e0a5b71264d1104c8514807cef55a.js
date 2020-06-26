4322eb7a882b2983b669683ad2f327ed54c66756
Siddhant Agnihotri <siddhant.ag7@gmail.com>
2020-06-26 16:46:46 +0530

    Updates

---
+++ precache-manifest.1d9e0a5b71264d1104c8514807cef55a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1dba35846d2a108ebc09b98b940fde",
+    "url": "/expense-graph/index.html"
+  },
+  {
+    "revision": "38121eceb8d8a7b5cc56",
+    "url": "/expense-graph/static/css/main.d06f5c9e.chunk.css"
+  },
+  {
+    "revision": "4200b5e92802446358a2",
+    "url": "/expense-graph/static/js/2.063b6141.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/expense-graph/static/js/2.063b6141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38121eceb8d8a7b5cc56",
+    "url": "/expense-graph/static/js/main.9a0dd2ed.chunk.js"
+  },
+  {
+    "revision": "f7ebfd541c1744319d7c",
+    "url": "/expense-graph/static/js/runtime-main.28ac7d85.js"
+  },
+  {
+    "revision": "c47b27e41a36c5b040ff07b85ad78d71",
+    "url": "/expense-graph/static/media/AvenirLTStd.c47b27e4.woff"
+  }
+]);
